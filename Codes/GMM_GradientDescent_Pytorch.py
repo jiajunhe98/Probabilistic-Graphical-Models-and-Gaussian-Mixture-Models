0c71a35cc53e0bf8b7b020698acd85ee7e095a04
Jiajun He <43494544+hejj16@users.noreply.github.com>
2021-04-03 21:56:21 +0200

    Add files via upload

--- Codes/GMM_GradientDescent_Pytorch.py
+++ Codes/GMM_GradientDescent_Pytorch.py
@@ -1,24 +1,24 @@
 import numpy as np
 import torch
 
-
 def dnorm(x, mu, sigma):
-
-    log_prob = -1 / 2 * (x - mu).T @ torch.inverse(sigma) @ (x - mu) \
+    log_prob = - 1 / 2 * torch.diag((x - mu).T @ torch.inverse(sigma) @ (x - mu)) \
                - 1 / 2 * torch.log(torch.det(sigma)) \
                - sigma.shape[0] / 2 * torch.log(torch.tensor(2 * np.pi))
-    return log_prob.item()
+    return log_prob
+
 
 def dGMM(x, pi, mus, sigmas):
 
-    log_probs = torch.zeros(len(mus))
+    log_probs = []
     for i in range(len(mus)):
-        log_probs[i] += dnorm(x, mus[i], sigmas[i]) + torch.log(pi)[i]
-    max_log = torch.max(log_probs)
-    log_probs = torch.exp(log_probs - max_log)
-    log_prob = torch.sum(log_probs) + max_log
+        log_probs.append(dnorm(x, mus[i], sigmas[i]) + torch.log(pi)[i])
+    log_probs = torch.vstack(log_probs)
+    max_log = torch.max(log_probs, dim=0, keepdim=True)[0]
+    probs = torch.exp(log_probs - max_log)
+    log_prob = torch.log(torch.sum(probs, dim=0, keepdim=True)) + max_log
 
-    return log_prob
+    return log_prob.reshape(-1)
 
 
 class GMM_GD:
@@ -46,7 +46,7 @@ class GMM_GD:
         self.mus = [None] * self.n_clusters
         self.sigmas = [None] * self.n_clusters
 
-    def fit(self, data, learning_rate=0.01, max_iter=100):
+    def fit(self, data, learning_rate=0.001, max_iter=5000, return_losses=True):
         """
         Fit the model to data.
 
@@ -57,37 +57,45 @@ class GMM_GD:
 
         """
 
-        # Initialization by K-means
+        # Initialization pi and means by K-means
         z = self._initialization(data)
-        self.pi = torch.tensor([np.mean(z == cluster)
+        self._pi_ = torch.tensor([np.log(np.mean(z == cluster))
                                 for cluster in range(self.n_clusters)], requires_grad=True).to(self.device)
+        self.pi = torch.exp(self._pi_) / torch.sum(torch.exp(self._pi_))
         self.mus = [torch.tensor(np.mean(data[z == cluster, :], axis=0).reshape(-1, 1), requires_grad=True).to(self.device)
                     for cluster in range(self.n_clusters)]
-        self.sigmas = [torch.tensor(np.cov(data[z == cluster, :].T), requires_grad=True).to(self.device)
+
+        # Initialization sigmas as unit matrix
+        self._sigmas_ = [torch.tensor(np.eye(data.shape[1]), requires_grad=True).to(self.device)
+                         for cluster in range(self.n_clusters)]
+        self.sigmas = [self._sigmas_[cluster] @ self._sigmas_[cluster].T
                        for cluster in range(self.n_clusters)]
 
         # Gradient Descent by Pytorch
-        optimizer = torch.optim.SGD(params=[self.pi] + self.mus + self.sigmas, lr=learning_rate)
-        lambd_pi = 1
-        lambd_sigmas = [1] * self.n_clusters
+        optimizer = torch.optim.SGD(params=[self._pi_] + self.mus + self._sigmas_, lr=learning_rate)
+        L = []
         for iter in range(max_iter):
-            optimizer.zero_grad()
-            old_pi = self.pi
-            log_likelihood = 0
-            for x in data:
-                x = torch.from_numpy(x.reshape(-1, 1)).to(self.device)
-                log_likelihood += dGMM(x, self.pi, self.mus, self.sigmas)
-            L = -log_likelihood + lambd_pi * torch.linalg.norm(self.pi)
-            for cluster in range(self.n_clusters):
-                L -= lambd_sigmas[cluster] * torch.det(self.sigmas[cluster])
-
-            lambd_pi += learning_rate * torch.linalg.norm(self.pi)
-            for cluster in range(self.n_clusters):
-                lambd_sigmas[cluster] = max(lambd_sigmas[cluster] + learning_rate * torch.det(self.sigmas[cluster]), 0)
-            L.backward()
+            x = torch.from_numpy(data.T).to(self.device)
+            minus_log_likelihood = torch.sum(-dGMM(x, self.pi, self.mus, self.sigmas))
+            minus_log_likelihood.backward()
+            L.append(minus_log_likelihood.item())
             optimizer.step()
-            if torch.linalg.norm(old_pi - self.pi) <= 1e-5:
+
+            grad_norm = np.linalg.norm(self._pi_.grad.detach()) \
+                        + sum([np.linalg.norm(self.mus[cluster].grad.detach())
+                               + np.linalg.norm(self._sigmas_[cluster].grad.detach())
+                               for cluster in range(self.n_clusters)])
+
+            optimizer.zero_grad()
+
+            self.pi = torch.exp(self._pi_) / torch.sum(torch.exp(self._pi_))
+            self.sigmas = [self._sigmas_[cluster] @ self._sigmas_[cluster].T
+                           for cluster in range(self.n_clusters)]
+
+            if grad_norm <= 1e-5:
                 break
+        if return_losses:
+            return L, self
         return self
 
     def predict(self, x):
@@ -107,7 +115,7 @@ class GMM_GD:
         z = np.argmax(log_prob, axis=0)
         return z
 
-    def _initialization(self, data, max_iter=50):
+    def _initialization(self, data, max_iter=10):
         """
         Initialization by K-Means.
         """
